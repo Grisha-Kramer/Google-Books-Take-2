0aba37981dcf5a478f6fbd700b40fe7bbfe76bcd
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-23 21:08:07 -0400

    Data is going somewhere now

--- client/src/BookCard.js
+++ client/src/BookCard.js
@@ -1,11 +1,15 @@
 import React from "react";
+import bookAPI from "./utils/API"
 
 const BookCard = (props) => {
+ function saveBook(data) {
+    bookAPI.saveBook(data)
+  }
   return (
     <div className="card-container">
         <img src={props.image} alt="" />
         <div className="desc">
-          <button type="button" className="btn btn-info">Save</button>
+          <button onClick={() => saveBook(props)} type="button" className="btn btn-info">Save</button>
   <h2>{props.title}</h2>
             <h3>{props.author}</h3>
             <p>{props.description}</p>
